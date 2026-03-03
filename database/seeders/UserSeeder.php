f4be8ebc6ac6d173b5b0f6211e20370e7ed37d8e
ammukarthi <ammukarthisakthi@gmail.com>
2026-03-03 20:52:49 +0530

    migration,model,seeder-done

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        User::factory()->count(2)->create([
+            'role' => 'admin'
+        ]);
+
+        User::factory()->count(3)->create([
+            'role' => 'organizer'
+        ]);
+
+        User::factory()->count(10)->create([
+            'role' => 'customer'
+        ]);
+    }
+}
